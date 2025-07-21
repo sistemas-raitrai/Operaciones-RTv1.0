c0a16e715700551d3681c3e2a7d6a5efb98b6b8c
Ignacio <sistemas@raitrai.cl>
2025-07-21 11:51:44 -0400

    Update grupos.js

--- grupos.js
+++ grupos.js
@@ -3,8 +3,7 @@ import { getAuth, onAuthStateChanged, signOut }
   from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js';
 import {
   collection, getDocs, query, orderBy,
-  doc, updateDoc, addDoc,
-  Timestamp
+  doc, updateDoc, addDoc, Timestamp
 } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js';
 
 const auth = getAuth(app);
