f7d75c6621594acb6b75766be50cbaf4a8525627
Ignacio <sistemas@raitrai.cl>
2025-08-06 11:39:48 -0400

    Update hoteles.js

--- hoteles.js
+++ hoteles.js
@@ -141,7 +141,7 @@ async function renderHoteles(){
       <h3>${toUpper(h.nombre)}</h3>
       <div class="subtitulo">DESTINO: ${toUpper(h.destino)}</div>
       <div class="subsubtitulo">
-        DISPONIBLE DURANTE ${fmtFecha(h.fechaInicio)} HASTA ${fmtFecha(h.fechaFin)}
+        DESDE ${fmtFecha(h.fechaInicio)} HASTA ${fmtFecha(h.fechaFin)}
       </div>`;
 
     // 2.2️⃣ Cada bloque de grupo asignado
