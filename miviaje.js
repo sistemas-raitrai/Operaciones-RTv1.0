5b1c297fd3188fa30ff3066f68b8875393ac6899
Ignacio <sistemas@raitrai.cl>
2025-10-22 10:48:50 -0300

    Change topCount from 4 to 3

--- miviaje.js
+++ miviaje.js
@@ -725,7 +725,7 @@ function renderItin(grupo, fechas, hideNotes, targetEl){
   wrap.appendChild(filaBottom);
 
   const n = diasSecs.length;
-  const topCount = Math.min(4, n);
+  const topCount = Math.min(3, n);
   const bottomCount = Math.max(0, n - topCount);
 
   filaTop.style.display = filaBottom.style.display = 'grid';
