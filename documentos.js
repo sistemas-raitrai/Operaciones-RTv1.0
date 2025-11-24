ecf686b24a205a7eaf8bb856d661cb827c588604
Ignacio <sistemas@raitrai.cl>
2025-11-24 09:46:57 -0300

    Update documentos.js

--- documentos.js
+++ documentos.js
@@ -1611,6 +1611,9 @@ function buildFinanzasDoc(grupo, abonos, coord, vouchersData){
             ${grupo ? `<span>TOTAL: ${grupo.cantidadGrupo}</span>` : ''}
           </div>
         </div>
+        <div class="finanzas-logo">
+          <img src="Logo Raitrai.png" alt="Turismo RaiTrai">
+        </div>
       </div>
 
       ${coordBlock}
