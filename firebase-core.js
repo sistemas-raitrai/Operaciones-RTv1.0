81fbf34bd78b4040f0d49c4c3b35c5e5f66c36a2
Ignacio <sistemas@raitrai.cl>
2026-04-27 11:58:32 -0400

    Update firebase-core.js

--- firebase-core.js
+++ firebase-core.js
@@ -1,6 +1,8 @@
-// firebase-core.js
-// Compatibilidad: redirige al inicializador oficial del sistema.
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js';
+import { initializeFirestore } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js';
+import { firebaseConfig } from './firebase-config.js';
 
-export { app, db, auth } from './firebase-init.js';
+export const app = initializeApp(firebaseConfig);
+export const db  = initializeFirestore(app, { experimentalAutoDetectLongPolling: true });
 
-console.log('firebase-core.js usa firebase-init.js');
+console.log('projectId:', app.options?.projectId); // debe mostrar "sist-op-rt"
